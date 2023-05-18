6fb1cd0a530967958413ca40c66a90d596d130d1
asally <a.salawu@outlook.com>
2023-05-18 09:41:43 +0000

    adding styles + inline styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <h1>the dojo blog</h1>
+    <nav className='navbar'>
+        <h1>The Dojo Blog</h1>
         <div className="links">
             <a href="/">Home</a>
-            <a href="/create">New Blog</a>
+            <a href="/create" style={{
+                color: 'white',
+                backgroundColor: '#f1356d',
+                borderRadius: '8px'
+            }}>New Blog</a>
         </div>
     </nav>
   )
