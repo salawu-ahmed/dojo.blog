a58c864caf43f5cc474d950d4a9780bb7781ac89
asally <a.salawu@outlook.com>
2023-05-18 10:26:09 +0000

    outputting a list

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { useState } from 'react'
 
 const Home = () => {
-    //let name = 'mario'
-    const [name, setName] = useState('mario')
-    const [age, setAge] = useState(25)
-    const handleClick = () => {
-        setName('luigi')
-        setAge(30)
-    }
+    const [blogs] = useState([
+        {title: "My new website", body: 'lorem ipsum....', author: 'mario', id:1},
+        {title: "Welcome party!", body: 'lorem ipsum....', author: 'roshi', id:2},
+        {title: "Web dev top tips", body: 'lorem ipsum....', author: 'luigi', id:3}
+    ])
 
   return (
     <div className="home">
-        <h2>Homepage</h2>
-        <p>{name} is {age} years old </p>
-        <button onClick={handleClick}>click me</button>
+        {
+            blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by: {blog.author}</p>
+                </div>
+            ))
+        }
     </div>
   )
 }
