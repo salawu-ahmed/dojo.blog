2e4d053e33ded07b07564c6cc5b5144ade99b0c5
asally <a.salawu@outlook.com>
2023-05-19 09:41:19 +0000

    controlled inputs & two way binding

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Create = () => {
+  const [title, setTitle] = useState('hello')
+  const [body, setBody] = useState('this is the body')
+  const [author, setAuthor] = useState('mario')
+
   return (
-    <div>Create a new blog post</div>
+    <div className='create'>
+      <h2>Create a new blog post</h2>
+      <form>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label>Blog body:</label>
+        <textarea 
+          required 
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>add blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
+    </div>
   )
 }
 
